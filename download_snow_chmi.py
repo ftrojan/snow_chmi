424ddbd46ea42c51773609b89b1787ff9c503871
ftrojan <ftrojan@deloittece.com>
2018-12-24 13:55:42 +0100

    First commit

---
+++ download_snow_chmi.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 24 13:40:49 2018
+
+@author: com-ftrojan
+"""
+
+import numpy as np
+import pandas as pd
+
+# <img src="http://www.hladiny.cz/snih/draw_image.php?stN=29766&amp;act=DG&amp;cN=3" width="760" title="Graf za posledních 7 dní" border="0">
